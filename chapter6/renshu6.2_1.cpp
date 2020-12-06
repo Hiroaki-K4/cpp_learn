f4ea5a72b6264ff538730dd9c3f8123eb37d17c9
Hiroaki-K4 <49no19@gmail.com>
2020-12-06 10:03:57 +0900

    edit

--- chapter6/renshu6.2_1.cpp
+++ chapter6/renshu6.2_1.cpp
@@ -12,25 +12,25 @@ public:
 	coord() { x = 0; y = 0; }
 	coord(int i, int j) { x = i; y = j; }
 	void get_xy(int &i, int &j) { i = x; j = y; }
-	coord operator+(coord &ob2);
-	coord operator+(int i);
+	coord operator*(coord &ob2);
+	coord operator/(coord &ob2);
 };
 
-coord coord::operator+(coord &ob2)
+coord coord::operator*(coord &ob2)
 {
 	coord temp;
 
-	temp.x = x + ob2.x;
-	temp.y = y + ob2.y;
+	temp.x = x * ob2.x;
+	temp.y = y * ob2.y;
 	return temp;
 }
 
-coord coord::operator+(int i)
+coord coord::operator/(coord &ob2)
 {
 	coord temp;
 
-	temp.x = x + i;
-	temp.y = y + i;
+	temp.x = x / ob2.x;
+	temp.y = y / ob2.y;
 	return temp;
 }
 
@@ -39,12 +39,12 @@ int main()
 	coord o1(10, 10), o2(5, 3), o3;
 	int x, y;
 
-	o3 = o1 + o2;
+	o3 = o1 * o2;
 	o3.get_xy(x, y);
-	cout << "(o1 + o2) X: " << x << ", Y: " << y << endl;
-
-	o3 = o1 + 100;
+	cout << "(o1 * o2) X: " << x << ", Y: " << y << endl;
+	
+	o3 = o1 / o2;
 	o3.get_xy(x, y);
-	cout << "(o1 + 100) X: " << x << ", Y: " << y << endl;
+	cout << "(o1 / o2) X: " << x << ", Y: " << y << endl;
 	return 0; 
 }
