1baa999efdb9dfdc5626188a8f5b9cf2811d9f43
Hiroaki-K4 <49no19@gmail.com>
2020-12-28 09:44:49 +0900

    edit

--- sample.cpp
+++ sample.cpp
@@ -8,17 +8,20 @@ using namespace std;
 
 int main()
 {
-	double x;
+	int x;
 
 	cout.precision(4);
-	cout << "      x   sqrt(x)    x**2" << endl;
+	cout << "      x   loge(x)    log10(x)" << endl;
 
-	for (x = 2.0; x <= 20.0; x++) {
-		cout.width(7);
+	for (x = 2; x <= 100; x++) {
+		cout.width(10);
+		cout.precision(5);
 		cout << x << "  ";
-		cout.width(7);
+		cout.width(10);
+		cout.precision(5);
 		cout << sqrt(x) << "  ";
-		cout.width(7);
+		cout.width(10);
+		cout.precision(5);
 		cout << x*x << endl;
 	}
 	return 0;
