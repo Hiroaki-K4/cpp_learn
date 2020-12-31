817d5c56462259d7eb240ee496d345295e1829da
Hiroaki-K4 <49no19@gmail.com>
2020-12-31 10:41:35 +0900

    edit

---
+++ chapter8/rei8.5_2.cp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+
+class coord {
+public:
+	int x, y;
+	coord() { x = 0; y = 0; }
+	coord(int i, int j) { x = i; y = j; }
+};
+
+ostream &operator<<(ostream &stream, coord ob)
+{
+	stream << ob.x << ", " << ob.y << endl;
+	return stream;
+}
+
+int main()
+{
+	coord a(1, 1), b(10, 23);
+	cout << a << b;
+	return 0;
+}
--- sample.cpp
+++ sample.cpp
@@ -7,8 +7,29 @@
 using namespace std;
 
 
+class strtype {
+	char *p;
+	int len;
+public:
+	strtype(char *ptr);
+	~strtype() { delete [] p; }
+	friend ostream &operator<<(ostream &stream, strtype &ob);
+};
+
+strtype::strtype(char *ptr)
+{
+	len = strlen(ptr) + 1;
+	p = new char [len];
+	if (!p) {
+		cout << "メモリ割り当て" << endl;
+		exit(1);
+	}
+	strcpy(p, ptr);
+}
+
 int main()
 {
-	cout << resetiosflags(ios::basefield) << setiosflags(ios::showbase | ios::hex) << 100 << endl;
+	strtype s1("This is a test."), s2("I like C++.");
+	cout << s1 << "\n" << s2;
 	return 0;
 }
