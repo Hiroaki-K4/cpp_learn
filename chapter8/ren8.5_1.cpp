a9865c4c347dca890d40acfd3b06874ca3d259b6
Hiroaki-K4 <49no19@gmail.com>
2020-12-31 10:59:30 +0900

    edit

---
+++ chapter8/ren8.5_1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+
+class strtype {
+	char *p;
+	int len;
+public:
+	strtype(char *ptr);
+	~strtype() { delete [] p; }
+	friend ostream &operator<<(ostream &stream, strtype &ob);
+};
+
+strtype::strtype(char *ptr)
+{
+	len = strlen(ptr) + 1;
+	p = new char [len];
+	if (!p) {
+		cout << "メモリ割り当て" << endl;
+		exit(1);
+	}
+	strcpy(p, ptr);
+}
+
+ostream &operator<<(ostream &stream, strtype &ob)
+{
+	stream << ob.p << endl;
+	return stream;
+}
+
+int main()
+{
+	strtype s1("This is a test."), s2("I like C++.");
+	cout << s1 << "\n" << s2;
+	return 0;
+}
