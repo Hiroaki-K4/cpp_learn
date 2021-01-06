961de53eace41937864f06120e79fc0a1f2744e8
Hiroaki-K4 <49no19@gmail.com>
2021-01-06 09:16:24 +0900

    edit

--- sample.cpp
+++ sample.cpp
@@ -7,44 +7,14 @@
 using namespace std;
 
 
-class pwr {
-	int base;
-	int exponent;
-	double result;
+class square {
+	int len;
 public:
-	pwr(int b, int e);
-	friend ostream &operator<<(ostream &stream, pwr ob);
-	friend istream &operator>>(istream &stream, pwr &ob);
+	square(int a) { len = a; }
+	friend ostream operator<<(ostream &stream, square ob);
 };
 
-pwr::pwr(int b, int e)
+friend ostream operator<<(ostream &stream, square ob)
 {
-	base = b;
-	exponent = e;
-
-	result = 1;
-	for ( ; e; e--) result = result * base;
-}
-
-ostream &operator<<(ostream &stream, pwr ob)
-{
-	stream << ob.result << endl;
-	return stream;
-}
-
-istream &operator>>(istream &stream, pwr &ob)
-{
-	stream >> ob.base >> ob.exponent;
-	pwr temp(ob.base, ob.exponent);
-	ob = temp;
-	return stream;
-}
-
-int main()
-{
-	pwr ob1(10, 2);
-	cout << ob1;
-	cin >> ob1;
-	cout << ob1;
-	return 0;
+	
 }
