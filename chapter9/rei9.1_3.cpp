cf906b2f6375aa26fe97051e1f445fc66a71c8c0
Hiroaki-K4 <49no19@gmail.com>
2021-01-09 11:41:14 +0900

    edit

--- chapter9/rei9.1_3.cpp
+++ chapter9/rei9.1_3.cpp
@@ -7,20 +7,3 @@
 using namespace std;
 
 
-istream &getpass(istream &stream)
-{
-	cout << "\a";
-	cout << "パスワードを入力: ";
-	return stream;
-}
-
-int main()
-{
-	char pw[80];
-
-	do {
-		cin >> getpass >> pw;
-	} while (strcmp(pw, "password"));
-	cout << "ログオン完了" << endl;
-	return 0;
-}
