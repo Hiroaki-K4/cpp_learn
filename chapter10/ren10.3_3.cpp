70c20d44a7074e56d7935f1441eb34963afbb3e9
Hiroaki-K4 <49no19@gmail.com>
2021-01-16 10:59:59 +0900

    edit

--- chapter10/ren10.3_3.cpp
+++ chapter10/ren10.3_3.cpp
@@ -8,16 +8,6 @@
 using namespace std;
 
 
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
-#include <ctime>
-using namespace std;
-
-
 class base {
 public:
     virtual void func()
--- sample.cpp
+++ sample.cpp
@@ -8,51 +8,91 @@
 using namespace std;
 
 
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
-#include <ctime>
-using namespace std;
-
-
-class base {
+class list {
 public:
-    virtual void func()
-    {
-        cout << "baseのfunc()を使う\n";
-    }
+    list *head;
+    list *tail;
+    list *next;
+    int num;
+    list() { head = tail = next = NULL; }
+    virtual void store(int i) = 0;
+    virtual int retrieve() = 0;
 };
 
-class derived1 : public base {
+class queue : public list {
 public:
-    // void func()
-    // {
-    //     cout << "derived1のfunc()を使う\n";
-    // }
+    void store(int i);
+    int retrieve();
 };
 
-class derived2 : public derived1 {
-public:
-    void func()
-    {
-        cout << "derived2のfunc()を使う\n";
-    }
-};
-
-int main()
+void queue::store(int i)
 {
-    base *p;
-    base ob1;
-    derived1 ob2;
-    derived2 ob3;
-    p = &ob1;
-    p->func();
-    p = &ob2;
-    p->func();
-    p = &ob3;
-    p->func();
-    return 0;
+    list *item;
+    item = new queue;
+
+    if (!item) {
+        cout << "メモリ割り当てエラー\n";
+        exit(1);
+    }
+    item->num = i;
+
+    if (tail) tail->next = item;
+    tail = item;
+    item->next = NULL;
+    if (!head) head = tail;
+}
+
+int queue::retrieve()
+{
+    int i;
+    list *p;
+
+    if (!head) {
+        cout << "リストは空です\n";
+        return 0;
+    }
+    i = head->num;
+    p = head;
+    head = head->next;
+    delete p;
+    return i;
+}
+
+class stack : public list {
+public:
+    void store(int i);
+    int retrieve();
+};
+
+void stack::store(int i)
+{
+    list *item;
+    item = new stack;
+
+    if (!item) {
+        cout << "メモリ割り当てエラー\n";
+        exit(1);
+    }
+    item->num = i;
+
+    if (head) item->next = head;
+    head = item;
+    if (!tail) tail = head;
+}
+
+int stack::retrieve()
+{
+    int i;
+    list *p;
+
+    if (!head) {
+        cout << "リストは空です\n";
+        return 0;
+    }
+
+    i = head->num;
+    p = head;
+    head = head->next;
+    delete p;
+    return i;
 }
