2f46a7564ccc3922e07fdcad2ca4186480c578a1
Hiroaki-K4 <49no19@gmail.com>
2021-01-17 20:57:36 +0900

    edit

--- sample.cpp
+++ sample.cpp
@@ -8,32 +8,15 @@
 using namespace std;
 
 
-template <class X> void swapargs(X &a, X &b)
+template <class X> X my_min(X a, X b)
 {
-    X temp;
-
-    temp = a;
-    a = b;
-    b = temp;
-}
-
-void swapargs(int a, int b)
-{
-    cout << "swapargs(int, int)の内部です\n";
+	if (a <= b) return a;
+	else return b;
 }
 
 int main()
 {
-    int i = 10, j = 20;
-    float x = 10.1, y = 23.3;
-
-    cout << "オリジナル i, j: " << i << ' ' << j << endl;
-    cout << "オリジナル x, y: " << x << ' ' << y << endl;
-
-    swapargs(i, j);
-    swapargs(x, y);
-
-    cout << "入れ替え後 i, j: " << i << ' ' << j << endl;
-    cout << "入れ替え後 x, y: " << x << ' ' << y << endl;
-    return 0;
+	cout << my_min(3, 4) << endl;
+	cout << my_min('c', 'a') << endl;
+	return 0;
 }
