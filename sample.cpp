a390fc18ef48c9ed34037c7fab41e518f8f5b9cb
Hiroaki-K4 <49no19@gmail.com>
2021-01-18 09:44:48 +0900

    edit

--- sample.cpp
+++ sample.cpp
@@ -8,42 +8,40 @@
 using namespace std;
 
 
-template <class data_t> class list {
-    data_t data;
-    list *next;
+#define SIZE 10
+
+template <class StackType> class stack {
+    StackType stck[SIZE];
+    int tos;
 public:
-    list(data_t d);
-    void add(list *node) { node->next = this; next = 0; }
-    list *getnext() { return next; }
-    data_t getdata() { return data; }
+    void init() { tos = 0; }
+    void push(StackType ch);
+    StackType pop();
 };
 
-template <class data_t> list<data_t>::list(data_t d)
+template <class StackType>
+void stack<StackType>::push(StackType ob)
 {
-    data = d;
-    next = 0;
+    if (tos==SIZE) {
+        cout << "スタックは一杯です\n";
+        return;
+    }
+    stck[tos] = ob;
+    tos++;
+}
+
+template <class StackType>
+StackType stack<StackType>::pop()
+{
+    if (tos==0) {
+        cout << "スタックは空です\n";
+        return 0;
+    }
+    tos--;
+    return stck[tos];
 }
 
 int main()
 {
-    list<char> start('a');
-    list<char> *p, *last;
-    int i;
-
-    last = &start;
     
-    for (i = 1; i < 26; i++) {
-        p = new list<char> ('a' + i);
-        p->add(last);
-        last = p;
-    }
-
-    p = &start;
-
-    while (p) {
-        cout << p->getdata();
-        p = p->getnext();
-    }
-    cout << "\n";
-    return 0;
 }
