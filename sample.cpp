7561a6d4a896cbaae7af01040a0f184b67e31afe
Hiroaki-K4 <49no19@gmail.com>
2021-01-19 22:35:35 +0900

    edit

--- sample.cpp
+++ sample.cpp
@@ -8,18 +8,15 @@
 using namespace std;
 
 
-void Xhandler(int test) throw(int, char, double)
-{
-    if (test == 0) throw test;
-    if (test == 1) throw 'a';
-    if (test == 2) throw 123.23;
+void Xhandler()
+    
 }
 
 int main()
 {
     cout << "開始\n";
 
-    try {
+    try { 
         Xhandler(1);
     }
 
