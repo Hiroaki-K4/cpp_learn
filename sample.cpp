45554c4eb32ae89fefae49e2e8c46296ae10f951
Hiroaki-K4 <49no19@gmail.com>
2021-01-19 21:30:32 +0900

    edit

--- sample.cpp
+++ sample.cpp
@@ -12,9 +12,14 @@ void Xhandler(int test)
 {
     try {
         if (test) throw test;
+        else throw "値は0";
     }
     catch (int i) {
         cout << "捕獲! Ex. #: " << i << endl;
+    }
+    catch (char const *str) {
+        cout << "文字列を捕獲: ";
+        cout << str << endl;
     }
 }
 
