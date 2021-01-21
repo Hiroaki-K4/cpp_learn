da55aa6554ddcf72658d27f4099c084c08bbcce6
Hiroaki-K4 <49no19@gmail.com>
2021-01-21 09:52:00 +0900

    edit

--- sample.cpp
+++ sample.cpp
@@ -8,15 +8,33 @@
 using namespace std;
 
 
+template <class X>
+X array_mode(X *data, int size)
+{
+    register int t, w;
+    X md, oldmd;
+    int count, oldcount;
+
+    oldmd = 0;
+    oldcount = 0;
+    for (t = 0; t < size; t++) {
+        md = data[t];
+        count = 1;
+        for (w = t + 1; w < size; w++)
+            if (md == data[w]) count++;
+        if (count > oldcount) {
+            oldmd = md;
+            oldcount = count;
+        }
+    }
+    return oldmd;
+}
+
 int main()
 {
-    double *p;
-
-    do {
-        p = new(nothrow) double[100000];
-        if (p) cout << "メモリ割り当て成功\n";
-        else cout << "メモリ割り当て失敗\n";
-    } while (p);
-
+    int i[] = { 1, 2, 3, 4, 2, 3, 2, 2, 1, 5 };
+    char *p = "this is a test";
+    cout << "iのモード" << array_mode(i, 10) << endl;
+    cout << "*pのモード" << array_mode(p, (int) strlen(p)) << endl;
     return 0;
 }
