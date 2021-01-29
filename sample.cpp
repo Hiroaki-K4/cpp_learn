1f2a316e263edde9b9df9b5e7a4efd1395d1c0a0
Hiroaki-K4 <49no19@gmail.com>
2021-01-29 22:58:41 +0900

    edit

--- sample.cpp
+++ sample.cpp
@@ -12,25 +12,12 @@
 using namespace std;
 
 
-int main()
-{
-    map<char, int> m;
-    int i;
+class word {
+    char str[20];
+public:
+    word() { strcpy(str, ""); }
+    word(char *s) { strcpy(str, s); }
+    char *get() { return str; }
+};
 
-    for (i = 0; i < 10; i++) {
-        m.insert(make_pair((char)'A' + i, i));
-    }
-
-    char ch;
-    cout << "キーを入力: ";
-    cin >> ch;
-
-    map<char, int>::iterator p;
-    p = m.find(ch);
-    if (p != m.end())
-        cout << p->second << endl;
-    else
-        cout << "キーはマップにない" << endl;
-
-    return 0;
-}
+bool operator
