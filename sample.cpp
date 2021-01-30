ffc1ed6b7c2368f91085855427e9b8bbbbefa795
Hiroaki-K4 <49no19@gmail.com>
2021-01-30 11:40:21 +0900

    edit

--- sample.cpp
+++ sample.cpp
@@ -16,25 +16,8 @@ using namespace std;
 
 int main()
 {
-    map<string, string> m;
-    int i;
-
-    m.insert(pair<string, string>("yes", "no"));
-    m.insert(pair<string, string>("up", "down"));
-    m.insert(pair<string, string>("left", "right"));
-    m.insert(pair<string, string>("good", "bad"));
-
-    string s;
-    cout << "単語を入力: ";
-    cin >> s;
-
-    map<string, string>::iterator p;
-
-    p = m.find(s);
-    if (p != m.end())
-        cout << "反対語: " << p->second << endl;
-    else
-        cout << "単語がマップにない\n";
-
+    list<string> l;
+    l.push_back("a");
+    cout << l << endl;
     return 0;
 }
